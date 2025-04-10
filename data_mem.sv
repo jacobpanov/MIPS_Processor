4568370df3e7f8833ffd42ca9e3ef6696e80d188
Jacob Panov <84049465+jacobpanov@users.noreply.github.com>
2025-04-10 23:41:40 +0000

    Bug fixes

--- data_mem.sv
+++ data_mem.sv
@@ -2,7 +2,7 @@
 // This module implements a data memory for a MIPS processor with read and write functionality.
 // data_mem.sv
 
-`include "defines.sv"
+`include "definitons.sv"
 
 module data_mem #(
     parameter MEM_CELL_SIZE = 8, // Width of each memory cell
